dcabe2e555b2b14aa51f7e23f2634b7eaedf5896
Mahabubul Hasan <mahabubul.hasan@streamstech.com>
2023-01-24 21:36:15 +0600

    Initial

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Pool, Client } = require('pg')
+const fs = require('fs/promises');
+const {ConvertToUnicode, IsAscii} = require('./bijoy2uni')
+require('dotenv').config();
+
+const client = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  max: 2  
+});
+
+function fetchAllDistinct(table, column){
+	const sql = `SELECT DISTINCT ${column} FROM ${table} WHERE address IS NOT NULL`
+	return client.query(sql)
+}
+
+function saveRow(table, column, ascii, unicode){
+	return `UPDATE ${table} SET ${column}='${unicode}' WHERE ${column}='${ascii}';\n`
+}
+
+async function replaceInDb(table, column){
+	const res = await fetchAllDistinct(table, column)
+	let sql  = '';
+	res.rows.forEach(r => {
+		if(!IsAscii(r[column])){
+			return
+		}
+
+		sql += saveRow(table, column, r[column], ConvertToUnicode('bijoy', r[column].trim()))				
+	})
+	
+	console.log(sql)
+	await fs.writeFile(`./sql/${table}_${column}.sql`, sql);
+}
+
+async function run(callback){
+	try{
+		// await client.connect()
+		await callback()	
+		await client.end()		
+	}catch(err){
+		console.log(err)
+	}
+}
+
+run(async ()=>{
+	
+	await replaceInDb('raw_data.astructure', 'address')	
+})
